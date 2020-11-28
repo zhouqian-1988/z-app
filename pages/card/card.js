dd2b885e13ab3ab67a7bc5bfb4b420be128593bc
周倩 <178028369@qq.com>
2020-11-28 21:55:34 +0800

    发送网络请求获取猫眼电影

--- pages/card/card.js
+++ pages/card/card.js
@@ -5,14 +5,48 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 准备数据
+    list:[
+      {name: '三国演义', price: 50},
+      {name: '西游记', price: 55},
+      {name: '红楼梦', price: 48},
+      {name: '水浒传', price: 60}
+    ],
+    year: 3,
+    msg: '你好',
+    num: 123,
+    hot: []
+  },
+  // 处理函数
+  updateHandler() {
+    this.setData({
+      msg: 'someone like you',
+      num: 789,
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    // 发送请求
+    wx.request({
+      method: "GET",
+      url:"https://wx.maoyan.com/mmdb/movie/v2/list/hot.json",
+      data: {
+        limit: 8,
+        offset: 0,
+        ct: "上海"
+      },
+      // 成功时的回调
+      success: (res) => {
+        console.log(res);
+        const hot = res.data.data.hot
+        this.setData({
+          hot: hot
+        })
+      }
+    })
   },
 
   /**
